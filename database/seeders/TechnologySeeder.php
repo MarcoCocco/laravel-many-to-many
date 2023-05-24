79a39b41094c89760dcffd75beb1eb7b2ceee38c
MarcoCocco <marcococco28@gmail.com>
2023-05-24 15:25:44 +0200

    feat: creata la tabella ponte project_technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Illuminate\support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +16,18 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $technologies = ['Python', 'JavaScript', 'Java', 'C++', 'C#', 'PHP', 'TypeScript', 'HTML', 'CSS/Sass', 'SQL'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name, '-');
+            $newTechnology->color = $faker->hexColor();
+
+            $newTechnology->save();
+        }
     }
 }
